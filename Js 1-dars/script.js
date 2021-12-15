34bcde572fc0d3444d52f137eb94ab3131bbbb95
shaxa9505 <87860218+shaxa9505@users.noreply.github.com>
2021-12-15 16:53:16 +0500

    js-dars
    js-dars

---
+++ Js 1-dars/script.js
@@ -0,0 +1,41 @@
+    
+    
+    // <!-- eng birinchi cansolda number bilan stringlani tuwuntirasan -->
+
+    var name1 = "Salom"
+    let name = "Shohrux" // buni nmaligini tuwuntirasan yani letti i kiyn uzgaruvchi nmaligini agar namedan oldin son yozsek xatoligini tuwuntirasan
+    // i kiyn let const var bulani hammasini tuwuntirasan
+
+    const number = 448.9
+
+    const gerb = 1992
+
+    let sub = "salom"
+
+    // indi ikranga chiqariwti kursatasan alert bn document.writeni
+    // alert(name1)
+
+    // i kiyn uzgaruvchilani qande uzgartirib yoziwti kursatasan
+    // name = "Meliboyev"
+
+    // alert() // 1 ekranga chiqarish
+    // document.write(name) // 2 ekranga chiqarish
+    // console.log(name);
+
+    let num1 = prompt("birinchi sonni yozing")
+    let num2 = prompt("ikkinchi sonni yozing")
+    // let num1 = +prompt("birinchi sonni yozing") agar +prompt yozsek string ucadi
+    // let num2 = +prompt("ikkinchi sonni yozing") agar +prompt yozsek string ucadi
+
+    // i kiyn promptni nimaligini tuwuntirasan bu kak inputga uxwagan
+
+    // alert(+num1 + +num2) // agar wunde qisek arifmetika iwledi dsan
+    alert(num1 + num2)
+
+    // i kiyn wtfjs wu saytga kirip javascripti xatolarini kursatasan
+
+    // i kiyn saytni kursatib buganindan kiyn operatorlarni kursatasan  yani INKREMENT va DEKREMENTni i kiyn POSTFIX va PREFIXniyam kursatasan
+    // i kiyn cansolda katta kichik true false i++ ++i i-- --i wulani hammasini kursatasan
+
+
+    
